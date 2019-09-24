a98c1da5e86c0e8fe58609d630790820924a1c01
coVdiing <113714806@qq.com>
2019-09-24 10:15:14 +0800

    [edit][service][ProductService] 新增了三个方法，为Category对象填充对应的Product对象

--- src/main/java/com/vi/tmall/service/ProductService.java
+++ src/main/java/com/vi/tmall/service/ProductService.java
@@ -2,14 +2,27 @@ package com.vi.tmall.service;
 
 import java.util.List;
 
+import com.vi.tmall.pojo.Category;
 import com.vi.tmall.pojo.Product;
 
 public interface ProductService {
-	//增删改查
-	void add(Product product);
-	void delete(int id);
-	void update(Product product);
-	Product get(int id);
-	List<Product> list(int cid);
-	void setFirstProductImage(Product p);
+    //增删改查
+    void add(Product product);
+
+    void delete(int id);
+
+    void update(Product product);
+
+    Product get(int id);
+
+    List<Product> list(int cid);
+
+    void setFirstProductImage(Product p);
+
+    //新增三个方法
+    void fill(Category category);
+
+    void fill(List<Category> categories);
+
+    void fillByRow(List<Category> categories);
 }
