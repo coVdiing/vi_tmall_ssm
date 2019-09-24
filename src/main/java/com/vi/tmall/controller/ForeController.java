10013c6c5d4f17c09a7fb80b49697e7033fbd5ce
coVdiing <113714806@qq.com>
2019-09-24 10:26:17 +0800

    【add】【controller】【ForeController】对应前台路径的控制器

---
+++ src/main/java/com/vi/tmall/controller/ForeController.java
@@ -0,0 +1,40 @@
+package com.vi.tmall.controller;
+
+import com.vi.tmall.pojo.Category;
+import com.vi.tmall.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * 对应前台路径的控制器
+ */
+@Controller
+@RequestMapping("")
+public class ForeController {
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    ProductService productService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    ProductImageService productImageService;
+    @Autowired
+    PropertyValueService propertyValueService;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderItemService orderItemService;
+
+    @RequestMapping("forehome")
+    public String home(Model model) {
+        List<Category> categories = categoryService.list();
+        productService.fill(categories);
+        productService.fill(categories);
+        return "fore/home";
+    }
+}
