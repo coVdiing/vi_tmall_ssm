4477881e3dd8205bf873f240ca4a186045e7c1ff
coVdiing <113714806@qq.com>
2019-09-24 11:25:44 +0800

    【eidt】【service】【ProductImageService】修改编码格式为UTF8

--- src/main/java/com/vi/tmall/service/ProductImageService.java
+++ src/main/java/com/vi/tmall/service/ProductImageService.java
@@ -5,17 +5,16 @@ import java.util.List;
 import com.vi.tmall.pojo.ProductImage;
 
 public interface ProductImageService {
-	// 表示单个图片
-	String type_single = "type_single";
-	// 表示详情图片
-	String type_detail = "type_detail";
+    // 鍗曚釜鍥剧墖绫诲瀷
+    String type_single = "type_single";
+    // 璇︽儏鍥剧墖绫诲瀷
+    String type_detail = "type_detail";
 
-	void add(ProductImage productImage);
+    void add(ProductImage productImage);
 
-	void delete(int id);
-	//根据id获取单张图片
-	ProductImage get(int id);
-	//批量获取产品图片，由于一种产品有两类图片
-	//，详情图片和小窗口的图片，所以需要根据类型判断
-	List<ProductImage> list(int pid, String type);
+    void delete(int id);
+
+    ProductImage get(int id);
+
+    List<ProductImage> list(int pid, String type);
 }
